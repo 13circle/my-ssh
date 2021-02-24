ac4f572a35634ae4a6dde2f08afc614a826ac9dd
13circle <13circle97@gmail.com>
2021-02-24 21:38:56 +0900

    Revert path & refactor script

--- install.sh
+++ install.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-base_dir="~/.myssh"
+shellrc_name=.${SHELL##*/}rc
+shellrc=$HOME/$shellrc_name
+
+base_dir="$HOME/.myssh"
+src_dir="$base_dir/src"
 script_dir="$base_dir/sshconnect"
 
 mkdir $base_dir 2> /dev/null
 mkdir $script_dir 2> /dev/null
-rm -f $base_dir/myssh
 
-git clone https://github.com/13circle/my-ssh $base_dir
-gcc $base_dir/myssh.c -o $base_dir/myssh
+rm -rf $src_dir
+git clone https://github.com/13circle/my-ssh $src_dir
+gcc $src_dir/myssh.c -o $src_dir/myssh
 
-if [ -z $(cat ~/.bashrc 2> /dev/null | grep myssh) ]; then
-  echo "alias myssh='$base_dir/myssh'" >> ~/.bashrc
+if [ -z "$(cat $shellrc 2> /dev/null | grep myssh)" ]; then
+  echo "" >> $shellrc
+  echo "alias myssh='$src_dir/myssh'  # Inserted by MySSH" >> $shellrc
+  echo "" >> $shellrc
+
+  echo "Now restart the shell or run 'source ~/$shellrc_name'"
 fi
 
