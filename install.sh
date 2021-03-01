827450407f10f2240ba8646d94e2ab50ac17a9c9
13circle <13circle97@gmail.com>
2021-03-01 23:08:08 +0900

    Refactor varaibles & add environment variable

--- install.sh
+++ install.sh
@@ -4,23 +4,21 @@ shellrc_name=.${SHELL##*/}rc
 shellrc=$HOME/$shellrc_name
 
 base_dir="$HOME/.myssh"
-src_dir="$base_dir/src"
-ssh_dir="$base_dir/sshconnect"
-keys_dir="$ssh_dir/keys"
-configs_dir="$ssh_dir/configs"
 
 mkdir $base_dir 2> /dev/null
-mkdir $ssh_dir 2> /dev/null
-mkdir $keys_dir 2> /dev/null
-mkdir $configs_dir 2> /dev/null
+mkdir "$base_dir/sshconnect" 2> /dev/null
+mkdir "$base_dir/sshconnect/keys" 2> /dev/null
+mkdir "$base_dir/sshconnect/configs" 2> /dev/null
 
-rm -rf $src_dir
-git clone https://github.com/13circle/my-ssh $src_dir
-gcc $src_dir/myssh.c -o $src_dir/myssh
+rm -rf "$base_dir/src"
+git clone https://github.com/13circle/my-ssh "$base_dir/src"
+gcc "$base_dir/src/myssh.c" -o "$base_dir/src/myssh"
 
 if [ -z "$(cat $shellrc 2> /dev/null | grep myssh)" ]; then
   echo "" >> $shellrc
-  echo "alias myssh='$src_dir/myssh'  # Inserted by MySSH" >> $shellrc
+  echo "# Inserted by MySSH" >> $shellrc
+  echo "export MYSSH_ROOT_DIR=$base_dir" >> $shellrc
+  echo "alias myssh='$base_dir/src/myssh'" >> $shellrc
   echo "" >> $shellrc
 
   echo "Now restart the shell or run 'source ~/$shellrc_name'"
