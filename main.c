8f25fe1fadcf1a8a8bef5f7d5a0ee880b92641ae
13circle <13circle97@gmail.com>
2021-03-14 23:54:55 +0900

    Handle wrong arguments - counts

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int ac, char *av[]) {
-  //
+  
+  // myssh -u USER_NAME [-r,--register|-o,--open] [CONFIG_NAME]
+  if (ac >= 3) {
+    fprintf(stderr, "Error: Wrong Arguments \n");
+    fprintf(stderr, "%s -u USER_NAME [-r,--register|-o,--open] [CONFIG_NAME] \n", av[0]);
+    return EXIT_FAILURE;
+  }
+
+  // ...
 
   return 0;
 }
