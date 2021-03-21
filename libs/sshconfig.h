c1376cf23b6a4009c28267a5ab6601c77bbc72de
13circle <13circle97@gmail.com>
2021-03-21 20:10:52 +0900

    Move implementation code to utils

--- libs/sshconfig.h
+++ libs/sshconfig.h
@@ -10,23 +10,4 @@ typedef struct _SSH_Config {
   char key[128];
 } Config;
 
-Config* create_config(const char *config_name, const char *host_addr, const char *user_id, const char *key) {
-  Config *new_node;
-  new_node = (Config *)malloc(sizeof(Config));
-  
-  new_node->config_name = (char *)malloc(sizeof(char) * (strlen(config_name) + 1);
-  strcpy(new_node->config_name, config_name);
-
-  new_node->host_addr = (char *)malloc(sizeof(char) * (strlen(host_addr) + 1));
-  strcpy(new_node->host_addr, host_addr);
-
-  new_node->user_id = (char *)malloc(sizeof(char) * (strlen(user_id) + 1));
-  strcpy(new_node->user_id, user_id);
-
-  new_node->key = (char *)malloc(sizeof(char) * (strlen(key) + 1));
-  strcpy(new_node->key, key);
-
-  return new_node;
-}
-
 #endif
