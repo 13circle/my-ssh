e36df4016678dff23d55012b4508441f4a676db2
13circle <13circle97@gmail.com>
2021-03-24 23:03:20 +0900

    Validate username

--- main.c
+++ main.c
@@ -10,6 +10,12 @@ int main(int ac, char *av[]) {
     return EXIT_FAILURE;
   }
 
+  if (strcmp(av[1], "-u")) {
+    fprintf(stderr, "Username not specified. \n");
+    fprintf(stderr, "%s -u USER_NAME [-r,--register|-o,--open] [CONFIG_NAME] \n", av[0]);
+    return EXIT_FAILURE;
+  }
+
   // ...
 
   return 0;
